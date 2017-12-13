11d1da6e073b006ab6db2d305fb0254b21c11c62
jaywelborn <jesse.welborn@gmail.com>
2017-12-13 17:08:31 +0100

    up through video #5

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
   render() {
+    let name = 'Jay';
+    let green = '#12FF12'
+    let headerStyle = {color: green, 'font-size': '2.5rem'};
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 style={headerStyle} className="App-title">Hello, {name}</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
